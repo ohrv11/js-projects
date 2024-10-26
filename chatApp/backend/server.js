32ca9ce743cdcdfa8be3052239c3c0c03becf735
Aiden <lustoiann@gmail.com>
2024-10-26 13:56:59 +0200

    fixed imports

--- chatApp/backend/server.js
+++ chatApp/backend/server.js
@@ -1,12 +1,20 @@
-const express = require("express");
-
+import express from "express";
+import dotenv from "dotenv"; 
+ //After installing the dotenv package, the server is now running on port 8000
 const app = express();
 
-const PORT = process.env.PORT || 5000; //Port value assigned in the .env file
+dotenv.config();
+//with nodemon, each time that a change is done the port will
+//keep running automatically unntil we stop it manually 
+
+const PORT = process.env.PORT || 5000; //The port value will be either the one asigned
+//in the .env file or the port 5000.
+
+//Port value assigned in the .env file
 
 app.get("/", (req, res) => {
     //root route http://localhost:5000/
     res.send("Hello :)");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
