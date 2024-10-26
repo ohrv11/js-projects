4852dc7ec3a863c1d7b4491aef69537ab21868dc
Aiden <lustoiann@gmail.com>
2024-10-26 13:45:22 +0200

    turning the port into a variable

--- chatApp/backend/server.js
+++ chatApp/backend/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000; //Port value assigned in the .env file
+
 app.get("/", (req, res) => {
     //root route http://localhost:5000/
     res.send("Hello :)");
